54bc23a1a871240ff8cf2186bbfa6f18e2564e24
azsgit <10238943+azsgit@users.noreply.github.com>
2025-08-04 02:34:51 +0900

    wip

--- export/dyn/xoxxox_libmem/libmem.py
+++ export/dyn/xoxxox_libmem/libmem.py
@@ -10,17 +10,17 @@ class OpeMem:
 
   # 機能：データを、メモリに書き込む
   @staticmethod
-  def setmem(target, keymem, memory):
-    memory[keymem] = target
-    return ""
+  def setmem(target, keymem):
+    LibMid.memory[keymem] = target
+    return target
 
   # 機能：メモリからデータを読み出す
   @staticmethod
-  def getmem(target, keymem, memory):
+  def getmem(target, keymem):
     try:
-      return memory[keymem]
-    except Exception as e:
+      return LibMid.memory[keymem]
+    except Exception:
       return target
 
-LibMid.dicprc["xoxxox.OpeMem.setmem"] = {"frm": "xoxxox_libmem.OpeMem.setmem", "arg": ["keydat"], "mem": ["keymem"], "syn": True}
-LibMid.dicprc["xoxxox.OpeMem.getmem"] = {"frm": "xoxxox_libmem.OpeMem.getmem", "arg": ["keydat"], "mem": ["keymem"], "syn": True}
+LibMid.dicprc["xoxxox.OpeMem.setmem"] = {"frm": "xoxxox_libmem.OpeMem.setmem", "arg": ["keydat"], "cnf": ["keymem"], "syn": True}
+LibMid.dicprc["xoxxox.OpeMem.getmem"] = {"frm": "xoxxox_libmem.OpeMem.getmem", "arg": ["keydat"], "cnf": ["keymem"], "syn": True}
